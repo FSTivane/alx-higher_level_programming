55ec16a3908289049866aa517e475b6c361527ae
FSTivane <ciscotivane@gmail.com>
2024-01-04 06:22:25 -0800

    hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-from add_0 import add
+    from add_0 import add
 
-a = 1
-b = 2
-addition = add(a, b)
-print("{} + {} = {}".format(a, b, addition))
+    a = 1
+    b = 2
+    
+    addition = add(a, b)
+    
+    print("{} + {} = {}".format(a, b, addition))
