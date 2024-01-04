9548009dc2827385dda8a9839f673c404fa773b2
FSTivane <ciscotivane@gmail.com>
2024-01-04 12:36:30 -0800

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,14 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     x = len(args)
 
-    print(f"{x} argument{'s' if x != 1 else ''} {'.'if x == 0 else ':'}")
+    if x == 0:
+        print(" arguments")
+    
+    elif x == 1:
+        print("1 argument:")
 
-    if x > 0:
-        for i, arg in enumerate(args, 1):
+    else:
+        print("{} arguments:".format(x))
+
+    for i, arg in enumerate(args, 1):
             print(f"{i}: {arg}")
