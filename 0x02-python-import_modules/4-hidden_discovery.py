ebffa980c7c1f364ed4f463b7fc08bbe6677cd6b
FSTivane <ciscotivane@gmail.com>
2024-01-05 04:37:08 -0800

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 
-if __name__ == "__name__":
-    import hidden_4
+import importlib.util
 
-    names = dir(hidden_4)
+def print_names_from_pyc(pyc_file):
+    spec = importlib.util.spec_from_file_location("hidden_4", pyc_file)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
 
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    # Get names from the module excluding those starting with '__'
+    names = [name for name in dir(module) if not name.startswith('__')]
+
+    # Print the sorted names
+    for name in sorted(names):
+        print(name)
+
+if __name__ == "__main__":
+    print_names_from_pyc('hidden_4.pyc')
+
